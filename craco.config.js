c0193f148f7d4fea616c1af0dd472c5a10e22071
liukaixin <15713857919@163.com>
2024-06-19 15:34:29 +0800

    fix: proxy

--- craco.config.js
+++ craco.config.js
@@ -16,7 +16,7 @@ module.exports = {
     proxy: {
       // 选项写法
       "/api": {
-        target: "http://lego.api.liukaixin.online", // 目标地址
+        target: "http://lego.api.lytt.fun", // 目标地址
         changeOrigin: true, // 开启代理，在本地创建一个虚拟服务器，然后发送请求的数据，会同时会收到请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
           "^/api": "/api", // 路径重写，移除路径中的/api
