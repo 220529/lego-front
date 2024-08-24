5f2102477674ac6436c00d3adb29f0b2e3bc4e2b
liukaixin <15713857919@163.com>
2024-08-24 14:56:11 +0800

    add log

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "@/components/header";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    console.log("useEffect....0824");
+  }, [])
   return (
     <div className="App">
       <Header />
